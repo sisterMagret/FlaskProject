5c920ff308f78f7c21f78f18909eb40fea23b55b
sisterMagret <wistler4u@gmail.com>
2021-05-27 04:24:42 +0100

    Flask project

---
+++ market/templates/script.js
@@ -0,0 +1,16 @@
+// let count = 3;
+// if (count == 4){
+//     console.log('count is 4');
+// } else if(count < 4){
+//     console.log('count is less than 4 ');
+//  }else {
+//     console.log('count is not 4');
+// }
+
+// for (let a = 0; a < b.length; a++) {
+//     const c = b[a];
+    
+// }
+
+let name = "bob";
+console.log(`hi ${name}`);
